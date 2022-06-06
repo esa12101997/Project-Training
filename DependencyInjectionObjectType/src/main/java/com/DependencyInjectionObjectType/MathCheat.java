b9457d96b647e10156b24eaeafaaa53f7aaf1fd7
esa12101997 <99493719+esa12101997@users.noreply.github.com>
2022-06-06 16:48:13 +0530

    Add files via upload

---
+++ DependencyInjectionObjectType/src/main/java/com/DependencyInjectionObjectType/MathCheat.java
@@ -0,0 +1,13 @@
+package com.DependencyInjectionObjectType;
+
+public class MathCheat {
+
+	MathCheat(){
+		System.out.println("Mathcheat constructor ");
+	}
+
+	public void mathCheathing() {
+		System.out.println("maths cheating started ");
+	}
+
+}
